9a492a0d76294fa14006d95edfe482e88e275521
Abner das Dores <abner.dores@gmail.com>
2014-02-23 16:46:48 -0300

    Testes das options com Some e None

---
+++ CSharpOptions.Testes/OptionTestes.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace CSharpOptions.Testes
+{
+    [TestFixture]
+    public class OptionTestes
+    {
+        [Test]
+        public void OptionNoneTeste()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "Robb Stark"
+            };
+
+            var optionPessoa = Option<Pessoa>.Create(pessoa);
+
+            var logradouro = optionPessoa
+                                    .Map(p => p.Endereco)
+                                    .Map(e => e.Cidade)
+                                    .GetOrElse("Não informado");
+
+            logradouro.Should().Be("Não informado");
+        }
+
+        [Test]
+        public void OptionSomeTeste()
+        {
+            var pessoa = new Pessoa
+            {
+                Nome = "Robb Stark",
+                Endereco = new Endereco
+                {
+                    Cidade = "Winterfell"
+                }
+            };
+
+            var optionPessoa = Option<Pessoa>.Create(pessoa);
+
+            var logradouro = optionPessoa
+                                    .Map(p => p.Endereco)
+                                    .Map(e => e.Cidade)
+                                    .GetOrElse("Não informado");
+
+            logradouro.Should().Be("Winterfell");
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public Endereco Endereco { get; set; }
+    }
+
+    public class Endereco
+    {
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+    }
+}
