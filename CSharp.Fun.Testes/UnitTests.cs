fa91dd0ff491b0a91fdbd8056ae3b2fd398c8453
Breno Ferreira <bferreira@windowslive.com>
2014-04-03 10:58:38 -0300

    unit type tests

---
+++ CSharp.Fun.Testes/UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharp.Fun.Testes
+{
+    public class UnitTests
+    {
+        [Test]
+        public void UnitInstanceIsUnique()
+        {
+            Unit.Instance.Should().Be(Unit.Instance);
+        }
+
+        [Test]
+        public void UnitEqualsUnit()
+        {
+            Unit.Instance.Equals(Unit.Instance).Should().BeTrue();
+        }
+
+        [Test]
+        public void UnitIsNotDifferentFromUnit()
+        {
+            (Unit.Instance != Unit.Instance).Should().BeFalse();
+        }
+
+        [Test]
+        public void EqualsOperatorReturnsTrue()
+        {
+            (Unit.Instance == Unit.Instance).Should().BeTrue();
+        }
+    }
+}
