ef79a4c7458b306e504a8733a7acd244bcd8c827
Breno Ferreira <bferreira@windowslive.com>
2014-05-07 09:16:22 -0300

    refactoring of equality methods

--- CSharp.Fun/Option/Option.cs
+++ CSharp.Fun/Option/Option.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 
 namespace CSharp.Fun
 {
-
-    public abstract class Option<T> : IEquatable<Option<T>>
+    public abstract class Option<T>
     {
         public bool HasValue { get; protected set; }
 
         public T Value { get; protected set; }
 
-        public abstract bool Equals(Option<T> other);
+        public static implicit operator Option<T> (Option option)
+        {
+            return new None<T>();
+        }
+    }
+
+    internal class Some<T> : Option<T>, IEquatable<Option<T>>
+    {
+        public Some(T value)
+        {
+            Value = value;
+            HasValue = true;
+        }
+
+        public bool Equals(Option<T> other)
+        {
+            return other.HasValue && Value.Equals(other.Value);
+        }
 
         public override bool Equals(object obj)
         {
@@ -26,28 +42,9 @@ namespace CSharp.Fun
                 return (HasValue.GetHashCode() * 397) ^ EqualityComparer<T>.Default.GetHashCode(Value);
             }
         }
-
-        public static implicit operator Option<T> (Option option)
-        {
-            return new None<T>();
-        }
     }
 
-    internal class Some<T> : Option<T>
-    {
-        public Some(T value)
-        {
-            Value = value;
-            HasValue = true;
-        }
-
-        public override bool Equals(Option<T> other)
-        {
-            return other.HasValue && Value.Equals(other.Value);
-        }
-    }
-
-    internal class None<T> : Option<T>
+    internal class None<T> : Option<T>, IEquatable<Option<T>>
     {
         public new T Value { get { throw new Exception("No value"); } }
 
@@ -56,17 +53,14 @@ namespace CSharp.Fun
             HasValue = false;
         }
 
-        public override bool Equals(Option<T> other)
+        public bool Equals(Option<T> other)
         {
             return !other.HasValue;
         }
 
         public override bool Equals(object obj)
         {
-            if (obj is Option)
-                return true;
-
-            return base.Equals(obj);
+            return obj is Option;
         }
 
         public override int GetHashCode()
